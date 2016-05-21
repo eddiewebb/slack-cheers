5f58df4bf5134340f99adfcaaef7d077022fe098
Eddie Webb <ollitech@gmail.com>
2016-05-21 08:36:34 -0400

    create initial record or update slack ID for nominators on recognition instead of using web api to pull missing IDs.  THis means folks will need to recognize others to have a complete record ;)

--- src/services/CheerService.php
+++ src/services/CheerService.php
@@ -13,6 +13,10 @@ class CheerService{
 		$this->text = $text;
 		$this->app = $app;
 
+		//always run this to keep username<>ID mapping up to date.
+		$this->updateNominatorInfo();
+
+
 		if(strpos($text,"@") === false){
 			$this->errorMessage = "No peers specified, see help";
 			return;
@@ -20,6 +24,7 @@ class CheerService{
 			$this->arguments = $this->parseArguments($text);
 		}
 	}
+
 
 	function createCheerResponseFromSlackCommand() {
 		$response = new stdClass();
@@ -34,14 +39,15 @@ class CheerService{
 			if( $peerName == $this->fromHandle || $peerName == "me" ){
 			    slackError($this->app,"Way to recognize yourself. No points awarded.");
 			}
+			
 
 			$peer = R::findOne('peer','handle = ?', [$peerName]);
 			if (empty($peer)){
 			    //first nomination, create
 			    $peer=R::dispense('peer');
 			    $peer->handle = $peerName;
-			    $peer->user_id = findIdForHandle($peerName);
 			}
+
 			$cheers = R::dispense('cheer');
 			$cheers->reason = $this->arguments->reason;
 			$cheers->from = $this->fromId;
@@ -59,31 +65,47 @@ class CheerService{
 	}
 
 
+	function updateNominatorInfo(){
+		//look up by slack ID first, in case we need to updte by  name
+		$nominator = R::findOne('peer','user_id = ?', [$this->fromId]);
+		if (empty($nominator)){
+			//else try to find by user name based on previous recognitions to provide ID
+			$nominator = R::findOne('peer','handle = ?', [$this->fromHandle]);
+		    if (empty($nominator)){
+				$nominator=R::dispense('peer');
+		    }   
+		}
+		$nominator->user_id = $this->fromId;
+		$nominator->handle = $this->fromHandle;
+		
+		R::store($nominator);
+	}
 
-function parseArguments($text){
-    $arguments = new stdClass();
-    $pattern = '/^((?:@\S+\s)+)(.*)$/';
-    $valid = preg_match($pattern, $text, $matches);
 
-    if($valid){
-        if(sizeof($matches[1]) > 0){
-	       	$arguments->peers = explode(" ", trim($matches[1]));
+	function parseArguments($text){
+	    $arguments = new stdClass();
+	    $pattern = '/^((?:@\S+\s)+)(.*)$/';
+	    $valid = preg_match($pattern, $text, $matches);
+
+	    if($valid){
+	        if(sizeof($matches[1]) > 0){
+		       	$arguments->peers = explode(" ", trim($matches[1]));
+		    }else{
+		    	$this->errorMessage = "User mention pattern is not valid.";
+		    }
+
+		    if( ! sizeof($matches[2]) > 1 ){
+		        $arguments->reason = "Being Awesome";
+		    }else{
+		    	$arguments->reason = $matches[2];
+		    }
 	    }else{
-	    	$this->errorMessage = "User mention pattern is not valid.";
+	    	$this->errorMessage = "Please use the pattern '@username [additional users..] Reason for cheers'";
 	    }
 
-	    if( ! sizeof($matches[2]) > 1 ){
-	        $arguments->reason = "Being Awesome";
-	    }else{
-	    	$arguments->reason = $matches[2];
-	    }
-    }else{
-    	$this->errorMessage = "Please use the pattern '@username [additional users..] Reason for cheers'";
-    }
-
-    
-    return $arguments;
-}
+	    
+	    return $arguments;
+	}
 
 }
 
